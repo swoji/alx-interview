e0da4bbfb9d5602dbca045f25ce975770d6908ea
swoji <davidookuku@gmail.com>
2023-07-26 20:32:12 +0300

    script that shows n rows of a pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,38 +1,24 @@
 #!/usr/bin/python3
-"""
-Defines function that returns a list of lists of integers
-representing the Pascal's triangle of n.
-Creates a list of lists of integers representing Pascal's triangle
+"""A script to determine pascal's triangle for any number"""
 
-parameters:
-    n [int]:
-        the number of rows of Pascal's triangle to recreate
-
-return:
-    [list of lists of ints]:
-        representation of Pascal's triangle
-"""
 
 def pascal_triangle(n):
-    if type(n) is not int:
-        raise TypeError("n must be an integer")
-
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     triangle = []
+
+    # return (trianlgle if n <= 0)
     if n <= 0:
         return triangle
-    previous = [1]
-    for row_index in range(n):
-        rowlist = []
-        if row_index == 0:
-            rowlist = [1]
-        else:
-            for i in range(row_index + 1):
-                if i == 0:
-                    rowlist.append(0 + previous[i])
-                elif i == (row_index):
-                    rowlist.append(previous[i - 1] + 0)
-                else:
-                    rowlist.append(previous[i - 1] + previous[i])
-        previous = rowlist
-        triangle.append(rowlist)
+    for i in range(n):
+        tempList = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                tempList.append(1)
+            else:
+                tempList.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(tempList)
+    # print(triangle)
     return triangle
