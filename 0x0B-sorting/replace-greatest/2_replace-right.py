5d9c4ad4d55219727f24ce8b246d680d6fcb4773
swoji <davidookuku@gmail.com>
2024-05-13 18:01:28 +0300

    reverse iteration

--- 0x0B-sorting/replace-greatest/2_replace-right.py
+++ 0x0B-sorting/replace-greatest/2_replace-right.py
@@ -3,7 +3,7 @@ from typing import List
 class Solution:
   def replaceElements(self, arr: List[int]) -> List[int]:
     # initial max = -1
-    # reverse itration
+    # reverse iteration
     # new max = max(oldmax, arr[i])
 
     rightMax = -1
