d86c3ee00b0db4be53004b1d86a5b9e819133c7a
swoji <davidookuku@gmail.com>
2024-05-14 15:42:01 +0300

    corrected output

--- 0x0B-sorting/static-arrays/0_remove-element.py
+++ 0x0B-sorting/static-arrays/0_remove-element.py
@@ -1,17 +1,17 @@
 from typing import List
 
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-      k = 0
+  def removeElement(self, nums: List[int], val: int) -> int:
+    k = 0
 
-      for i in range(len(nums)):
-        if nums[i] != val:
-          nums[k] = nums[i]
-          k += 1
+    for i in range(len(nums)):
+      if nums[i] != val:
+        nums[k] = nums[i]
+        k += 1
 
-      return k
+    return k
 
 sol = Solution()
 
-print(sol.removeElement([0,1,2,2,3,0,4,2], 2)) # output: 5clear
+print(sol.removeElement([0,1,2,2,3,0,4,2], 2)) # output: 5
 
